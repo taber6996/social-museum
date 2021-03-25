c3f3e6d8542b1ef7fca9074a828fad9cb9875b6c
andysaull <80455606+andysaull@users.noreply.github.com>
2021-03-25 10:43:51 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+	<!-- Indice de vinculos -->
+		<nav class="navigation">
+			<ul>
+				<li><a href="index.html">Index</a></li>
+				<li><a href="bocetos.html">Bocetos</a></li>
+				<li><a href="contacto.html">Contacto</a></li>
+				<li><a href="detalles.html">Detalles</a></li>
+				<li><a href="miembros.html">Miembros</a></li>
+				<li><a href="planificacion.html">Planificacion</a></li>
+			</ul> 
+	
+		</nav>
+		<h1>Social Museum</h1>
+</header>
