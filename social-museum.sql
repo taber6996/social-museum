5d52e899d30f8b81c01931496ff374ddfc13bfcc
taber6996 <37449462+taber6996@users.noreply.github.com>
2021-04-10 16:41:15 +0200

    Add files via upload

---
+++ social-museum.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-04-2021 a las 19:48:06
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `social-museum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dibujo`
+--
+
+CREATE TABLE `dibujo` (
+  `id_dibujo` int(11) NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `id_autor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `exposicion`
+--
+
+CREATE TABLE `exposicion` (
+  `id_expo` int(11) NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `es_premium` tinyint(1) DEFAULT NULL,
+  `activa` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra`
+--
+
+CREATE TABLE `obra` (
+  `id_obra` int(11) NOT NULL,
+  `titulo` varchar(25) DEFAULT NULL,
+  `id_autor` int(11) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `likes` int(11) DEFAULT NULL,
+  `id_exposicion` int(11) DEFAULT NULL,
+  `img` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `unidades` int(11) DEFAULT NULL,
+  `img` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puja`
+--
+
+CREATE TABLE `puja` (
+  `id_puja` int(11) NOT NULL,
+  `id_propietario` int(11) NOT NULL,
+  `id_obra` int(11) NOT NULL,
+  `valor_ini` double NOT NULL,
+  `valor_actual` double NOT NULL,
+  `fecha_exp` date NOT NULL,
+  `ultimo_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nick` varchar(25) DEFAULT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `passwd` varchar(25) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `es_admin` tinyint(1) DEFAULT NULL,
+  `es_artista` tinyint(1) DEFAULT NULL,
+  `es_premium` tinyint(1) DEFAULT NULL,
+  `es_juez` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id_venta` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `dibujo`
+--
+ALTER TABLE `dibujo`
+  ADD PRIMARY KEY (`id_dibujo`),
+  ADD KEY `FK_DibujoUsuario` (`id_autor`);
+
+--
+-- Indices de la tabla `exposicion`
+--
+ALTER TABLE `exposicion`
+  ADD PRIMARY KEY (`id_expo`);
+
+--
+-- Indices de la tabla `obra`
+--
+ALTER TABLE `obra`
+  ADD PRIMARY KEY (`id_obra`),
+  ADD KEY `FK_ObraUsuario` (`id_autor`),
+  ADD KEY `FK_ObraExpo` (`id_exposicion`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- Indices de la tabla `puja`
+--
+ALTER TABLE `puja`
+  ADD PRIMARY KEY (`id_puja`),
+  ADD KEY `FK_PujaUsuario` (`id_propietario`),
+  ADD KEY `FK_PujaUsuario2` (`ultimo_usuario`),
+  ADD KEY `FK_ObraPuja` (`id_obra`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `FK_VentaUsuario` (`id_usuario`),
+  ADD KEY `FK_VentaProd` (`id_producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `dibujo`
+--
+ALTER TABLE `dibujo`
+  MODIFY `id_dibujo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `exposicion`
+--
+ALTER TABLE `exposicion`
+  MODIFY `id_expo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `obra`
+--
+ALTER TABLE `obra`
+  MODIFY `id_obra` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `puja`
+--
+ALTER TABLE `puja`
+  MODIFY `id_puja` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dibujo`
+--
+ALTER TABLE `dibujo`
+  ADD CONSTRAINT `FK_DibujoUsuario` FOREIGN KEY (`id_autor`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `obra`
+--
+ALTER TABLE `obra`
+  ADD CONSTRAINT `FK_ObraExpo` FOREIGN KEY (`id_exposicion`) REFERENCES `exposicion` (`id_expo`),
+  ADD CONSTRAINT `FK_ObraUsuario` FOREIGN KEY (`id_autor`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `puja`
+--
+ALTER TABLE `puja`
+  ADD CONSTRAINT `FK_ObraPuja` FOREIGN KEY (`id_obra`) REFERENCES `obra` (`id_obra`),
+  ADD CONSTRAINT `FK_PujaUsuario` FOREIGN KEY (`id_propietario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `FK_PujaUsuario2` FOREIGN KEY (`ultimo_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `FK_VentaProd` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
+  ADD CONSTRAINT `FK_VentaUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
