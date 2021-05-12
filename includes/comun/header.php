be76319cfb21e04510279bb82ccf8bccbcbbd232
andysaull <80455606+andysaull@users.noreply.github.com>
2021-05-12 07:26:03 +0200

    Add files via upload

--- includes/comun/header.php
+++ includes/comun/header.php
@@ -1,16 +1,18 @@
 <header>
-	<a href="index.php"> 
-		<img src="img/logo.jpg" >
-	</a>
-	<nav id="Menu Principal">
-		<ul id="lista-menu-principal">
-			<!-- <li> <img src="img/busca.jpg" width="15px" height="15px"> </li> --> 
-			<li> | <a href="artistas.php">Artistas</a></li>
-			<li> | <a href="expos.php">Expos</a></li>
-			<li> | <a href="tienda.php">Tienda</a></li>
-			<li> | <a href="eventos.php">Eventos</a></li>
-			<li> | <a href="cuenta.php">Cuenta</a></li>
-			<!-- <li> | <img src="img/spanish.jpg" width="15px" height="15px"> </li> -->
-		</ul>
+	<div id ="logo">
+		<a href="index.php"> 
+			<!-- <img src="img/logo.jpg" > -->
+			<img id= "logoSVG" src="img/logo.svg" alt="Social Museum Logo" />
+		</a>
+	</div>
+	
+	<nav id="menu-principal">
+		<!-- <li> <img src="img/busca.jpg" width="15px" height="15px"> </li> --> 
+		<a href="artistas.php">Artistas</a>
+		<a href="expos.php">Expos</a>
+		<a href="tienda.php">Tienda</a>
+		<a href="eventos.php">Eventos</a>
+		<a href="cuenta.php">Cuenta</a>
+		<!-- <li> | <img src="img/spanish.jpg" width="15px" height="15px"> </li> -->
 	</nav>	
 </header>
