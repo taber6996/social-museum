ba1c55f20ad0e4a692a6fe891ae6eb162b766298
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-13 14:24:20 +0200

    Update general_user.php

--- general_user.php
+++ general_user.php
@@ -1,20 +1,14 @@
 <?php 
 require_once __DIR__.'/includes/config.php';
 
+$usuario = $_SESSION['user'];
+$htmlDatosPersonales = $usuario->datosPersonales();
+
 $tituloPagina = "Cuenta - ";
 
 $contenidoPrincipal = <<<EOS
-<nav id="menu-secundario">
-<a href="general_user.php">General</a></li>
-<a href="subscrpciones.php">Subscripciones</a></li>
-<a href="entradas.php">Entradas</a></li>
-<a href="estudio.php">Estudio</a></li>
-<a href="compras.php">Compras</a></li>
-<a href="buzon.php">Buzón</a></li>
-<a href="logout.php">Cerrar sesión</a></li>
-</nav>
 <h1>Mi cuenta</h1>
-<p>Aquí estarían contenido de la cuenta</p>
 EOS;
+$contenidoPrincipal .=$htmlDatosPersonales;
 		
-require ("includes/plantillas/layout1.php");
+require ("includes/plantillas/layout2.php");
