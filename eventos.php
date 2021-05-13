901769a3b2079f208946ea8f041d40298ea6d59d
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-14 00:21:57 +0200

    Update eventos.php

--- eventos.php
+++ eventos.php
@@ -1,25 +1,6 @@
 <?php 
 require_once __DIR__.'/includes/config.php';
 
-$tituloPagina = 'Eventos - ';
-
-$mostrador = new es\ucm\fdi\aw\MostradorEventos();
-$htmlMostradorEventos = $mostrador->muestra();
-$tipo = "Concurso";
-$vocal = "o";
-if(isset($_GET["tipo"])){
-	$tipo = $_GET["tipo"];
-	if($tipo == "Subasta"){
-		$vocal = "a";
-	}
-}
-$contenidoPrincipal=<<<EOS
-<nav id="menu-secundario">
-<a href="eventos.php?tipo=Concurso">Concursos</a>
-<a href="eventos.php?tipo=Subasta">Subastas</a>
-</nav>	
-
-$htmlMostradorEventos
-EOS;
+header('Location: concursos.php');
 
 require __DIR__.'/includes/plantillas/layout1.php';
