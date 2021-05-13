1d9de9629cebe1c77416c9cb49027f570d0b6720
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-14 00:46:22 +0200

    Update concursos.php

--- concursos.php
+++ concursos.php
@@ -3,23 +3,15 @@ require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Concursos - ';
 
-$mostrador = new es\ucm\fdi\aw\MostradorEventos();
-$htmlMostradorEventos = $mostrador->muestra();
-$tipo = "Concurso";
-$vocal = "o";
-if(isset($_GET["tipo"])){
-	$tipo = $_GET["tipo"];
-	if($tipo == "Subasta"){
-		$vocal = "a";
-	}
-}
+$mostrador = new es\ucm\fdi\aw\MostradorConcursos();
+$htmlMostradorConcursos = $mostrador->muestra();
+
 $contenidoPrincipal=<<<EOS
 <nav id="menu-secundario">
 <a href="concursos.php">Concursos</a>
 <a href="subastas.php">Subastas</a>
 </nav>	
-
-$htmlMostradorEventos
+$htmlMostradorConcursos
 EOS;
 
 require __DIR__.'/includes/plantillas/layout1.php';
