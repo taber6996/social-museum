9230c6b4f32a386c79c384446733f14ab61c2c15
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-13 14:25:02 +0200

    Update general_admin.php

--- general_admin.php
+++ general_admin.php
@@ -4,14 +4,8 @@ require_once __DIR__.'/includes/config.php';
 $tituloPagina = "Cuenta - ";
 
 $contenidoPrincipal = <<<EOS
-<nav id="menu-secundario">
-<a href="general_admin.php">General</a></li>
-<a href="productos.php">Subir producto</a></li>
-<a href="lista_eventos.php">Crear evento</a></li>
-<a href="logout.php">Cerrar sesión</a></li>
-</nav>
 <h1>Mi cuenta</h1>
 <p>Aquí estarían contenido de la cuenta de un administrador</p>
 EOS;
 		
-require ("includes/plantillas/layout1.php");
+require ("includes/plantillas/layout2.php");
