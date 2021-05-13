29f5a987d981b44bdac21e7365a361a41b4b57a8
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-13 14:32:42 +0200

    Update 03-datos.sql

--- mysql/03-datos.sql
+++ mysql/03-datos.sql
@@ -8,10 +8,10 @@ TRUNCATE TABLE `Productos`;
   artist@gmail.com: artistpass
   
 */
-INSERT INTO `Usuarios` (`id`, `correo`, `nombre`, `rol`, `password`) VALUES
-(1, 'admin@gmail.com', 'Administrador', 'admin', '$2y$10$j3gDDnUmICg/rvP0lmz8Duv2FcE1Ufi0tDQpIqx5cKcbqtkBOxhfS'),
-(2, 'artist@gmail.com', 'Artista', 'artist', '$2y$10$rYYIYCUzVJqTAFEhRZV7R.4DZF6hOGuVezEP592V/Su4Jq1D1.KEa'),
-(3, 'user@gmail.com', 'Usuario', 'user', '$2y$10$ImLgzNnDkWlI7LBB5a1mk.vNu8Fb8z79syAsoOXqM7jy5hrTaZKnG');
+INSERT INTO `Usuarios` (`id`, `correo`, `nombre`, `rol`, `password`, `avatar`) VALUES
+(1, 'admin@gmail.com', 'Administrador', 'admin', '$2y$10$j3gDDnUmICg/rvP0lmz8Duv2FcE1Ufi0tDQpIqx5cKcbqtkBOxhfS',0),
+(2, 'artist@gmail.com', 'Artista', 'artist', '$2y$10$rYYIYCUzVJqTAFEhRZV7R.4DZF6hOGuVezEP592V/Su4Jq1D1.KEa',0),
+(3, 'user@gmail.com', 'Usuario', 'user', '$2y$10$ImLgzNnDkWlI7LBB5a1mk.vNu8Fb8z79syAsoOXqM7jy5hrTaZKnG',0);
 
 INSERT INTO `Obras` (`id`, `id_autor`, `titulo`, `descripcion`, `likes`) VALUES
 (1, 2, 'Perro', 'Perro jugando', 25),
