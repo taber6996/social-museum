ae62d6eee8339bb95cc4bf07396879cd321e8143
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-05-14 00:20:43 +0200

    Add files via upload

---
+++ crear_evento.php
@@ -0,0 +1,16 @@
+<?php 
+require_once __DIR__.'/includes/config.php';
+
+$form = new es\ucm\fdi\aw\FormularioEvento();
+$htmlFormEvento = $form->gestiona();
+//$mostrador = new es\ucm\fdi\aw\MostradorEventos();
+//$htmlListaEventos = $mostrador->muestra();
+
+$tituloPagina = 'Social Museum';
+
+$contenidoPrincipal = <<<EOS
+	<h3>Crear nuevo evento</h3>
+	$htmlFormEvento
+	EOS;
+
+require __DIR__.'/includes/plantillas/layout2.php';
