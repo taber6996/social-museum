f15629ec92f012ce2e235348a2501a42cc48433e
taber6996 <37449462+taber6996@users.noreply.github.com>
2021-06-09 20:32:07 +0200

    Add files via upload

---
+++ vistaDibujos.php
@@ -0,0 +1,26 @@
+<?php 
+require_once __DIR__.'/includes/config.php';
+
+$tituloPagina = 'Subastas - ';
+
+/* VARIABLES PARA DEJAR MARCADO EL MENU */
+$menuArtistas = "";
+$menuExpos = "";
+$menuTienda = "";
+$menuEventos = "active";
+$menuCuenta = "";
+
+$mostrador = new es\ucm\fdi\aw\MostradorDibujos();
+$htmlMostradorDibujos = $mostrador->muestra();
+
+$contenidoPrincipal=<<<EOS
+<nav id="menu-secundario">
+<a href="concursos.php">Concursos</a>
+<a href="subastas.php">Subastas</a>
+</nav>
+<div class="tarjetasSubastas">
+$htmlMostradorDibujos
+</div>
+EOS;
+
+require __DIR__.'/includes/plantillas/layout1.php';
