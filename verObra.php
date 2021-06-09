e86619a313d14f4bfb20905b398e0632b497634b
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-06-09 14:04:51 +0200

    se puede comentar fotos

--- verObra.php
+++ verObra.php
@@ -1,10 +1,13 @@
 <?php 
 require_once __DIR__.'/includes/config.php';
 
-//$id_obra = $_GET['obra'];
 
 if(isset($_GET['obra'])){
-$id_obra = $_GET['obra'];	
+	$id_obra = $_GET['obra'];
+	$_SESSION['obra'] = $id_obra;
+
+}else{
+	$id_obra = $_SESSION['obra'];
 }
 
 $tituloPagina = $id_obra." - ";
@@ -17,10 +20,28 @@ $menuEventos = "";
 $menuCuenta = "";
 
 $mostradorO= new es\ucm\fdi\aw\MostradorObra($id_obra);
-$MostradorObra = $mostradorO->muestraObra($id_obra);
+$MostradorObra = $mostradorO->muestraObra();
+$htmlComentarios = $mostradorO->muestraComentarios();
+
+$forularioC= new es\ucm\fdi\aw\FormularioComentario($id_obra);
+$htmlFormComentario = $forularioC->gestiona();
 
 $contenidoPrincipal=<<<EOS
+<div id="verObra">
 $MostradorObra
+</div>
+EOS;
+if (isset($_SESSION["login"]) && $_SESSION["login"]) {	
+$contenidoPrincipal .=<<<EOS
+<div id="espacioComentario">
+$htmlFormComentario
+</div>
+EOS;
+}
+$contenidoPrincipal .=<<<EOS
+<div id="comentarios">
+$htmlComentarios
+</div>
 
 EOS;
 
