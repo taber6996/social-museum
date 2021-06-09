46d57f57ae93ad6dbebd9860dd653b22e19e5718
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-06-09 06:51:09 +0200

    Add files via upload

--- premium.php
+++ premium.php
@@ -1,19 +1,27 @@
 <?php
 require_once ("includes/config.php");
 
-$formC = new es\ucm\fdi\aw\FormularioCambiaDatosUsuario();
-$htmlFormC = $formC->gestiona();
-$formA = new es\ucm\fdi\aw\FormularioAvatar();
-$htmlFormA = $formA->gestiona();
-
 $tituloPagina = "Cuenta - ";
+
+$form1 = new es\ucm\fdi\aw\GestionAltaPremium();
+$htmlAltaPremium = $form1->gestiona();
+$htmlInfoAlta = $form1->infoAlta();
+$form2 = new es\ucm\fdi\aw\GestionBajaPremium();
+$htmlBajaPremium = $form2->gestiona();
+$htmlInfoBaja = $form2->infoBaja();
 
 $contenidoPrincipal = "";
 
+if(isset($_SESSION['premium']) && $_SESSION['premium']){
 $contenidoPrincipal .= <<<EOS
-<h1>Premium</h1>
+$htmlInfoBaja
+$htmlBajaPremium
 EOS;
-
-
+}else{
+$contenidoPrincipal .= <<<EOS
+$htmlInfoAlta
+$htmlAltaPremium
+EOS;
+}
 
 require ("includes/plantillas/layout1.php");
