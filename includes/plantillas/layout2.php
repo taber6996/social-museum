3294008a8e3c266c21bdb8d71f573c559105b9d4
isabelroman <73533367+isabelroman@users.noreply.github.com>
2021-06-09 14:07:14 +0200

    Add files via upload

--- includes/plantillas/layout2.php
+++ includes/plantillas/layout2.php
@@ -27,6 +27,16 @@
 
 <body>
 
+<script>
+window.onload=function(){
+	var pos=window.name || 0;
+	window.scrollTo(0,pos);
+}
+window.onunload=function(){
+	window.name=self.pageYOffset || (document.documentElement.scrollTop+document.body.scrollTop);
+}
+</script>
+
 	<?php
     require("includes/comun/header.php");
     
